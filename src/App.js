9ebb9431502b5dd5c336cae86e33264e7de52ba0
Juan Carlos Mercado <barkatu.83@gmail.com>
2020-04-10 13:31:01 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Navbar from "./components/Navbar";
+import LinkForm from "./pages/LinkForm";
+import TopLinks from "./pages/TopLinks";
+import OpenLink from "./pages/OpenLink";
+import Container from '@material-ui/core/Container';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container maxWidth="sm">
+        <Navbar />
+        <Switch>
+          <Route path="/top-links" exact><TopLinks /></Route>
+          <Route path="/:slug" exact><OpenLink /></Route>
+          <Route path="/" exact><LinkForm /></Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
